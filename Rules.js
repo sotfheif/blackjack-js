4699f9de2a7d22f3b643e7bec13d8b7f83865aa8
sotfheif <vovmart98@gmail.com>
2023-08-27 16:18:12 +0400

    add rules

---
+++ Rules.js
@@ -0,0 +1,8 @@
+export class Rules {
+    deckCount = 1
+    s17 = true // dealer must stand soft 17
+    //mb add surrender
+    //mb add double down
+    //mb add insurance
+    //mb add deck refresh frequency. currently refreshes every game     
+}
