c9a591f5ed73c963c35edcc93afc3028bec51ea8
sotfheif <vovmart98@gmail.com>
2023-08-27 20:15:30 +0400

    add Game start and checkBlackjack methods

--- Game.js
+++ Game.js
@@ -1,6 +1,35 @@
-export class Game{
-    constructor() {
+import {Player} from "./Player.js"
+import {Deck} from "./Deck.js"
 
+export class Game{
+    constructor(rules) {
+        rules = this.rules
     }
-    
+    player
+    dealer
+    deck
+    static build(rules) {
+        return new this(rules)
+    }
+    start(){
+        player = Player.build(false)
+        dealer = Player.build(true)
+        deck = Deck.build()
+        this.player.drawFirstCards(this.deck)
+        this.dealer.drawFirstCards(this.deck)
+        this.checkBlackjack()
+    }
+    checkBlackjack(){
+        const playerBlackjack = this.player.hasBlackjack()
+        const dealerBlackjack = this.dealer.hasBlackjack()
+        if (playerBlackjack && dealerBlackjack) {
+            console.log("Draw, both players have blackjack.")
+        } else if (playerBlackjack) {
+            console.log("Blackjack, you won!")
+        } else if (dealerBlackjack) {
+            console.log("Dealer blackjack, you lost.")
+        } else {
+
+        }
+    }
 }
