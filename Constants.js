82b7a689b8dba79c956b88438d844a2c4defba20
sotfheif <vovmart98@gmail.com>
2023-08-31 03:39:37 +0400

    add maps to translate card props into file name

--- Constants.js
+++ Constants.js
@@ -34,8 +34,26 @@ export const Ranks = Object.freeze({
     both: 3
   })
 
+  export const suitFileName = new Map([[Suits.Clubs, "clubs"],
+  [Suits.Diamonds, "diamonds"],[Suits.Hearts, "hearts"],
+  [Suits.Spades, "spades"]])
 
-
+  export const rankFileName = new Map([
+    [Ranks.ace, "ace"],
+    [Ranks.two, 2],
+    [Ranks.three, 3],
+    [Ranks.four, 4],
+    [Ranks.five, 5],
+    [Ranks.six, 6],
+    [Ranks.seven, 7],
+    [Ranks.eight, 8],
+    [Ranks.nine, 9],
+    [Ranks.ten, 10],
+    [Ranks.jack, "jack"],
+    [Ranks.queen, "queen"],
+    [Ranks.king, "king"]
+  ])
+  
   export const MAX_SCORE = 21
 
   
